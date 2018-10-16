5d58eba4838425ffbaeece86bc578712b8d8f834
Katrina Pritchard <pritckat@gmail.com>
2018-10-16 18:32:21 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,3 +1,8 @@
 function hideWhenClicked() {
-  
+  var x = document.getElementById("hideMe");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
